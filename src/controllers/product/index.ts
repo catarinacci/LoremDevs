38f7f2f0d16272f67918e800140780f781969a51
catarinacci <catarinacci@gmail.com>
2025-04-02 14:15:22 -0300

    Add endpoint get product

--- src/controllers/product/index.ts
+++ src/controllers/product/index.ts
@@ -3,12 +3,14 @@ import product from "../../models/product";
 import { validateProduct } from "../../helpers/validateProduct";
 
 export const getProduct = async(req: Request, res: Response, next: NextFunction)=>{
-    const data = req.params.data
+    const data = req.query.data as string
 
     //compruebo si es un id o name para poder realizar la bsqueda
-    const product = await validateProduct(res,data)
+    const product = await validateProduct(data)
 
-    res.json({
-        products: product
-    })
+    res.status(200).json({
+        success: true,
+        product: product,
+        
+      });
 }
